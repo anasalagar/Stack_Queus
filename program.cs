1472886dcbab818b67152780d350b9221070fef4
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-11-01 22:16:46 +0530

    program.cs file added

---
+++ program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace StackLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object stack class
+            StackLinkedList list = new StackLinkedList();
+            //calling push method
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            //calling display method
+            list.Display();
+            //calling IsEmpty method
+            list.IsEmpty();
+            list.Display();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
