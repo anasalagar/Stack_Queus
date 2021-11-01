12b0f69fc4565239e2dc5afe4cb61446739c6eea
anasalagar <90851784+anasalagar@users.noreply.github.com>
2021-11-01 23:27:57 +0530

    program.cs file added

---
+++ program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace QueueLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object stack class
+            QueueLinkedList queue = new QueueLinkedList();
+            //calling Enquue method
+            queue.Enqueve(56);
+            queue.Enqueve(30);
+            queue.Enqueve(70);
+            //calling display method
+            queue.Display();
+             Console.ReadLine();
+
+        }
+    }
+}
